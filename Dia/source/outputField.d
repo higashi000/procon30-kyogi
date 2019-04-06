275691c5c7dc82bc52bbb7ffa6ca80073ab520d0
higashi000 <higashi.ict@gmail.com>
2019-04-06 22:42:13 +0900

    [change] output format to `Field.csv`

--- Dia/source/outputField.d
+++ Dia/source/outputField.d
@@ -6,8 +6,14 @@ void outputFieldDate(int[][] fieldDate)
 {
   auto outputFile = File("Field.csv", "w");
 
-  foreach (col; 0 .. fieldDate.length)
-    outputFile.writeln(fieldDate[col]);
+  foreach (col; 0 .. fieldDate.length) {
+    foreach (line; 0 .. fieldDate[col].length) {
+      outputFile.write(fieldDate[col][line]);
+      if (line != fieldDate[col].length - 1)
+        outputFile.write(" ");
+    }
+    outputFile.writeln;
+  }
 
   outputFile.detach();
 }
