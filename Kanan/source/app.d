094aee83613f946cf58caa44ef4803f88448aa2c
higashi000 <higashi.ict@gmail.com>
2019-05-28 15:14:15 +0900

    [add] 2手先まで読むようにした, [change] dx, dyを `greedyAlgorithm` のローカル変数にした

--- Kanan/source/app.d
+++ Kanan/source/app.d
@@ -1,22 +1,42 @@
 import std.stdio;
 import Kanan.tile;
 import Kanan.greedy;
+import Kanan.color;
 
 void main()
 {
-  Tile[][] Field = new Tile[][](3, 3);
+  Tile[][] Field = new Tile[][](5, 5);
 
-  foreach (i; 0 .. 3) {
-    foreach (j; 0 .. 3) {
+  foreach (i; 0 .. 5) {
+    foreach (j; 0 .. 5) {
       Field[i][j].tilePoint = (i + 1) * (j + 1);
     }
   }
-
-  Field[1][1].tilePoint = 1000;
 
   int[] whichDir = new int[2];
   int[] whichAgent = [0, 0];
   greedyAlgorithm(Field, whichAgent, whichDir);
 
+  Field[0][0].color = Color.Blue;
+
+  foreach (i; 0 .. 5) {
+    write("|");
+    foreach (j; 0 .. 5) {
+      dispTile(Field[i][j]);
+      write("|");
+    }
+    writeln;
+  }
+
   writeln(whichDir);
+}
+
+void dispTile(Tile tile) {
+  if (tile.color == Color.White) {
+    writef("\033[0;47m%3d\033[0;39m", tile.tilePoint);
+  } else if (tile.color == Color.Red) {
+    writef("\033[0;41m%3d\033[0;39m", tile.tilePoint);
+  } else if (tile.color == Color.Blue) {
+    writef("\033[0;44m%3d\033[0;39m", tile.tilePoint);
+  }
 }
--- Kanan/source/greedy.d
+++ Kanan/source/greedy.d
@@ -1,24 +1,39 @@
 module Kanan.greedy;
 
-import Kanan.tile;
-
-int[] dx = [-1, -1, 0, 1, 1, 1, 0, -1];
-int[] dy = [0, -1, -1, -1, 0, 1, 1, 1];
+import Kanan.tile : Tile;
 
 void greedyAlgorithm(Tile[][] Field, int[] whichAgent, ref int[] whichDir) {
+  import std.range : zip;
+  import std.algorithm : sort;
+  static immutable int[] dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  static immutable int[] dy = [0, -1, -1, -1, 0, 1, 1, 1];
+  int[] dir = [0, 1, 2, 3, 4, 5, 6, 7];
+  int[] point = new int[8];
   int max = -17;
+
   foreach (i; 0 .. 8) {
+    int nowPoint = Field[whichAgent[0]][whichAgent[1]].tilePoint;
     if (!trueDir(whichAgent, Field[0].length, Field.length, i))
       continue;
-    if (Field[whichAgent[0] + dx[i]][whichAgent[1] + dy[i]].tilePoint > max) {
-      max = Field[whichAgent[0] + dx[i]][whichAgent[1] + dy[i]].tilePoint;
-      whichDir[0] = whichAgent[0] + dx[i];
-      whichDir[1] = whichAgent[1] + dy[i];
+    nowPoint += Field[whichAgent[0] + dx[i]][whichAgent[1] + dy[i]].tilePoint;
+    foreach (j; 0 .. 8) {
+      int[2] nowAgentPos = [whichAgent[0] + dx[i], whichAgent[1] + dy[i]];
+      if (!trueDir(nowAgentPos, Field[0].length, Field.length, j))
+        continue;
+      nowPoint += Field[whichAgent[0] + dx[i] + dx[j]][whichAgent[1] + dy[i] + dy[j]].tilePoint;
+      if (nowPoint > max) {
+        whichDir[0] = dx[i];
+        whichDir[1] = dy[i];
+        max = nowPoint;
+      }
     }
   }
 }
 
 bool trueDir(int[] whichAgent, ulong fieldX, ulong fieldY, int dir) {
+  static immutable int[] dx = [-2, -1, 0, 1, 1, 1, 0, -1];
+  static immutable int[] dy = [0, -1, -1, -1, 0, 1, 1, 1];
+
   if (whichAgent[0] + dx[dir] < 0)
     return false;
   else if (fieldX <= whichAgent[0] + dx[dir])
