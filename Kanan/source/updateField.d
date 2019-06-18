3c76f6a03fa86b942e25a9d38c5bb107cd344fc3
higashi000 <higashi.ict@gmail.com>
2019-06-18 15:51:08 +0900

    [add] エージェントの位置とタイルの色を更新する関数

---
+++ Kanan/source/updateField.d
@@ -0,0 +1,30 @@
+module Kanan.updateField;
+
+import Kanan.tile;
+import Kanan.agent;
+import Kanan.color;
+import std.stdio;
+
+void updateAgentColor(ref Tile[][] Field, ref int[] whereAgent, int[] whichDir) {
+  Color* nowPosColor = &Field[whereAgent[1]][whereAgent[0]].color;
+  Color* nextPosColor = &Field[whereAgent[1] + whichDir[1]][whereAgent[0] + whichDir[0]].color;
+  Agent* nowPosAgent = &Field[whereAgent[1]][whereAgent[0]].agent;
+  Agent* nextPosAgent = &Field[whereAgent[1] + whichDir[1]][whereAgent[0] + whichDir[0]].agent;
+
+  *nextPosAgent = *nowPosAgent;
+  *nowPosAgent = Agent.Null;
+  whereAgent[0] += whichDir[0];
+  whereAgent[1] += whichDir[1];
+
+  if (*nextPosColor == Color.White) {
+    *nextPosColor = *nowPosColor;
+  }
+  else if (*nextPosColor == Color.Blue) {
+    if (*nowPosAgent == Agent.Red)
+      *nextPosColor = Color.White;
+  }
+  else if (*nextPosColor == Color.Red) {
+    if (*nowPosAgent == Agent.Blue)
+      *nextPosColor = Color.White;
+  }
+}
