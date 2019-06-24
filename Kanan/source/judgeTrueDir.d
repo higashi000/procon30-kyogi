33c7d60e164ddcfa6248ff11143c7e7dc4548481
higashi000 <higashi.ict@gmail.com>
2019-06-25 06:49:35 +0900

    [fix] スペルミス

--- Kanan/source/judgeTrueDir.d
+++ Kanan/source/judgeTrueDir.d
@@ -1,16 +1,16 @@
 module Kanan.judgeTrueDir;
 
-bool trueDir(int[] whichAgent, ulong fieldX, ulong fieldY, int dir) {
+bool trueDir(int[] whereAgent, ulong fieldX, ulong fieldY, int dir) {
   static immutable int[] dx = [-1, -1, 0, 1, 1, 1, 0, -1];
   static immutable int[] dy = [0, -1, -1, -1, 0, 1, 1, 1];
 
-  if (whichAgent[0] + dx[dir] < 0)
+  if (whereAgent[0] + dx[dir] < 0)
     return false;
-  else if (fieldX <= whichAgent[0] + dx[dir])
+  else if (fieldX <= whereAgent[0] + dx[dir])
     return false;
-  else if (whichAgent[1] + dy[dir] < 0)
+  else if (whereAgent[1] + dy[dir] < 0)
     return false;
-  else if (fieldY <= whichAgent[1] + dy[dir])
+  else if (fieldY <= whereAgent[1] + dy[dir])
     return false;
 
   return true;
