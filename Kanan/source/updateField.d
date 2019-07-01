c98a2e5293313f15b0040159c8e17bdba5492aab
higashi000 <higashi.ict@gmail.com>
2019-07-01 17:07:13 +0900

    [add] 領域外を参照した際にそのエージェントを動かさない

--- Kanan/source/updateField.d
+++ Kanan/source/updateField.d
@@ -7,11 +7,24 @@ import Kanan.judgeTrueDir;
 import std.stdio;
 
 void updateAgentColor(ref Tile[][] Field, ref int[] whereAgent, int[] whichDir) {
+  static immutable int[] dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  static immutable int[] dy = [0, -1, -1, -1, 0, 1, 1, 1];
+
+  int dir;
+  foreach (i; 0 .. 8) {
+    if (dx[i] == whichDir[0] && dy[i] == whichDir[1])
+      dir = i;
+  }
+
+  if (!trueDir(whereAgent, Field[0].length, Field.length, dir)) {
+    writeln("wrong direction");
+    return;
+  }
+
   Color* nowPosColor = &Field[whereAgent[1]][whereAgent[0]].color;
   Color* nextPosColor = &Field[whereAgent[1] + whichDir[1]][whereAgent[0] + whichDir[0]].color;
   Agent* nowPosAgent = &Field[whereAgent[1]][whereAgent[0]].agent;
   Agent* nextPosAgent = &Field[whereAgent[1] + whichDir[1]][whereAgent[0] + whichDir[0]].agent;
-
 
   *nextPosAgent = *nowPosAgent;
   *nowPosAgent = Agent.Null;
