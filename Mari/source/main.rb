81213e4833ddaea1b4757ed61477848b90dd36a1
higashi000 <higashi.ict@gmail.com>
2019-07-04 15:28:10 +0900

    [add] ローカルのjsonファイルをhashにした

--- Mari/source/main.rb
+++ Mari/source/main.rb
@@ -1,1 +1,17 @@
-print("Hello World!")
+require 'json'
+require 'msgpack/rpc'
+
+jsonData = open("main.json") do |i|
+  JSON.load(i)
+end
+
+connectGUI = MessagePack::RPC::Client.new('localhost', 8080)
+
+fieldX = jsonData["width"]
+fieldY = jsonData["height"]
+fieldPoint = jsonData["points"]
+fieldColor = jsonData["tiled"]
+
+myTeamID = jsonData["teams"][0]["teamID"]
+myAgentData = jsonData["teams"][0]["agents"][0].to_a
+rivalAgentData = jsonData["teams"][1]["agents"]
