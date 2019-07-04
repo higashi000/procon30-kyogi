c202ec96da07eb866523e3a698ed94e5aafca077
higashi000 <higashi.ict@gmail.com>
2019-07-04 21:27:03 +0900

    [add] エージェントの座標とIDを配列で管理するようにした

--- Mari/source/main.rb
+++ Mari/source/main.rb
@@ -13,5 +13,25 @@ fieldPoint = jsonData["points"]
 fieldColor = jsonData["tiled"]
 
 myTeamID = jsonData["teams"][0]["teamID"]
-myAgentData = jsonData["teams"][0]["agents"][0].to_a
-rivalAgentData = jsonData["teams"][1]["agents"]
+#myAgentData = jsonData["teams"][0]["agents"][0]
+
+myAgentData = []
+rivalAgentData = []
+
+jsonData["teams"][0]["agents"].size.times do |i|
+  myAgentData.push([])
+  jsonData["teams"][0]["agents"][i].each_value do |j|
+    myAgentData[i].push(j)
+  end
+end
+
+jsonData["teams"][1]["agents"].size.times do |i|
+  rivalAgentData.push([])
+  jsonData["teams"][1]["agents"][i].each_value do |j|
+    rivalAgentData[i].push(j)
+  end
+end
+
+
+p myAgentData
+p rivalAgentData
