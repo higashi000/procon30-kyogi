429522404fed068bec2ad3a4db894e881f889439
higashi000 <higashi.ict@gmail.com>
2019-07-12 23:36:16 +0900

    [change] jsonをparseするところをrubyからgoに変更

---
+++ Mari/source/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+  "strconv"
+)
+
+type FieldData struct {
+	Width             int     `json:"width"`
+	Height            int     `json:"height"`
+	Points            [][]int `json:"points"`
+	StartedAtUnixTime int     `json:"startedAtUnixTime"`
+	Turn              int     `json:"turn"`
+	Tiled             [][]int `json:"tiled"`
+	Teams             []struct {
+		TeamID int `json:"teamID"`
+		Agents []struct {
+			AgentID int `json:"agentID"`
+			X       int `json:"x"`
+			Y       int `json:"y"`
+		} `json:"agents"`
+		TilePoint int `json:"tilePoint"`
+		AreaPoint int `json:"areaPoint"`
+	} `json:"teams"`
+	Actions []interface{} `json:"actions"`
+}
+
+func main() {
+  convertData := convertJsonToSendData()
+
+  fmt.Println(convertData)
+}
+
+func getFieldData() {
+  // サーバーにリクエスト送るやつ
+  // そのうち書く
+}
+
+func sendAgentActions() {
+  // エージェントの行動送るやつ
+  // そのうち書く
+}
+
+func convertJsonToSendData() string {
+  var fieldData FieldData
+
+  bytes, err := ioutil.ReadFile("main.json")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  if err := json.Unmarshal(bytes, &fieldData); err != nil {
+    log.Fatal(err)
+  }
+
+  var convertData string
+
+  convertData += strconv.Itoa(fieldData.Width)
+  convertData += "\n"
+
+  convertData += strconv.Itoa(fieldData.Height)
+  convertData += "\n"
+
+  for i := 0; i < fieldData.Height; i++ {
+    for j := 0; j < fieldData.Width; j++ {
+      convertData += strconv.Itoa(fieldData.Points[i][j])
+      convertData += ","
+    }
+    convertData += "\n"
+  }
+
+  convertData += strconv.Itoa(fieldData.StartedAtUnixTime)
+  convertData += "\n"
+
+  convertData += strconv.Itoa(fieldData.Turn)
+  convertData += "\n"
+
+  for i := 0; i < fieldData.Height; i++ {
+    for j := 0; j < fieldData.Width; j++ {
+      convertData += strconv.Itoa(fieldData.Tiled[i][j])
+      convertData += ","
+    }
+    convertData += "\n"
+  }
+
+  for i := 0; i < 2; i++ {
+    convertData += strconv.Itoa(fieldData.Teams[i].TeamID)
+    convertData += "\n"
+    convertData += strconv.Itoa(len(fieldData.Teams[i].Agents))
+    convertData += "\n"
+
+    for j := 0; j < len(fieldData.Teams[i].Agents); j++ {
+      convertData += strconv.Itoa(fieldData.Teams[i].Agents[j].AgentID)
+      convertData += "\n"
+      convertData += strconv.Itoa(fieldData.Teams[i].Agents[j].X)
+      convertData += "\n"
+      convertData += strconv.Itoa(fieldData.Teams[i].Agents[j].Y)
+      convertData += "\n"
+    }
+
+    convertData += strconv.Itoa(fieldData.Teams[i].TilePoint)
+    convertData += "\n"
+    convertData += strconv.Itoa(fieldData.Teams[i].AreaPoint)
+    convertData += "\n"
+  }
+
+  return convertData
+}
