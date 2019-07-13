3cbdcca581b26ae028a08485f9cb712335d0b25b
higashi000 <higashi.ict@gmail.com>
2019-07-13 11:11:52 +0900

    [add] フィールドデータの取得

--- Kanan/source/connector.d
+++ Kanan/source/connector.d
@@ -1,22 +1,44 @@
 module Kanan.connector;
 
 import Kanan.rsvData;
+import std.socket;
+import std.conv;
 import std.stdio;
 
 class KananConnector {
-  rsvClientData rsvTemp;
 
-  void getMariData(int[][] fieldColor, int[][] myAgentData, int[][] rivalAgentData)
-  {
-    rsvTemp.fieldColor = fieldColor;
-    rsvTemp.myAgentData = myAgentData;
-    rsvTemp.rivalAgentData = rivalAgentData;
-
-    foreach (i; 0 .. rsvTemp.fieldColor.length) {
-      foreach (j; 0 .. rsvTemp.fieldColor[0].length) {
-        write(rsvTemp.fieldColor[i][j], " ");
-      }
-      writeln;
-    }
+  this(string ip, ushort port) {
+    this.ip = ip;
+    this.port = port;
   }
+
+  this() {
+    this.ip = "127.0.0.1";
+    this.port = 8080;
+  }
+
+  private {
+    string rsvFieldData;
+    string ip;
+    ushort port;
+  }
+
+  void getFieldData()
+  {
+    auto socket = new TcpSocket(new InternetAddress(ip, port));
+
+    socket.send("Solver");
+
+    ubyte[4018] rsvData;
+    ulong size = socket.receive(rsvData);
+
+    if (size > 0) {
+      rsvFieldData = cast(string)rsvData[0 .. size];
+    }
+
+    write(rsvFieldData);
+
+    socket.close();
+  }
+
 }
