8d490576b9a3b0b9b6781383c8aee287d2f7a2b7
higashi000 <higashi.ict@gmail.com>
2019-07-13 11:20:23 +0900

    [add] 受け取るデータの追加

--- Kanan/source/rsvData.d
+++ Kanan/source/rsvData.d
@@ -1,11 +1,19 @@
 module Kanan.rsvData;
 
-struct rsvClientData {
-  int fieldX;
-  int fieldY;
-  int[][] fieldPoint;
-  int[][] fieldColor;
+struct rsvMariData {
+  int width;
+  int height;
+  int[][] point;
+  int startedAtUnixTime;
+  int turn;
+  int[][] color;
+  int agentNum;
   int myTeamID;
   int[][] myAgentData;
+  int myTilePoint;
+  int myAreaPoint;
+  int rivalTeamID;
   int[][] rivalAgentData;
+  int rivalTilePoint;
+  int rivalAreaPoint;
 }
