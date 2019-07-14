199c43fa672644606cc1540597cc93c63399b10d
higashi000 <higashi.ict@gmail.com>
2019-07-14 22:43:32 +0900

    [add] 受け取ったデータを構造体に格納するようにした(エージェントの数まで)

--- Kanan/source/connector.d
+++ Kanan/source/connector.d
@@ -4,6 +4,7 @@ import Kanan.rsvData;
 import std.socket;
 import std.conv;
 import std.stdio;
+import std.string : split;
 
 class KananConnector {
 
@@ -36,9 +37,42 @@ class KananConnector {
       rsvFieldData = cast(string)rsvData[0 .. size];
     }
 
-    write(rsvFieldData);
-
     socket.close();
   }
 
+  rsvMariData parseFieldData(int myTeamID)
+  {
+    rsvMariData parseRsvData;
+    auto parseData = rsvFieldData.split("\n");
+    int dataPos = 0;
+
+    rsvFieldData.writeln;
+
+    parseRsvData.width = (parseData[0].split(" "))[0].to!int;
+    parseRsvData.height = (parseData[0].split(" "))[1].to!int;
+
+    parseRsvData.point = new int[][](parseRsvData.height, parseRsvData.width);
+    foreach (i; 0 .. parseRsvData.height) {
+      auto line = (parseData[1].split(";"))[i];
+      foreach (j; 0 .. parseRsvData.width) {
+        parseRsvData.point[i][j] = (line.split(" "))[j].to!int;
+      }
+    }
+
+    parseRsvData.startedAtUnixTime = parseData[2].to!int;
+    parseRsvData.turn = parseData[3].to!int;
+
+    parseRsvData.color = new int[][](parseRsvData.height, parseRsvData.width);
+    foreach (i; 0 .. parseRsvData.height) {
+      auto line = (parseData[4].split(";"))[i];
+      foreach (j; 0 .. parseRsvData.width) {
+        parseRsvData.color[i][j] = (line.split(" "))[j].to!int;
+      }
+    }
+
+    parseRsvData.agentNum = parseData[5].to!int;
+
+    writeln(parseRsvData);
+    return parseRsvData;
+  }
 }
