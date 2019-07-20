4accde5cd07f6d447015cf62b1d0b56ffa4287f1
higashi000 <higashi.ict@gmail.com>
2019-07-20 14:45:04 +0900

    [add] 領域ポイントの計算

--- Kanan/source/field.d
+++ Kanan/source/field.d
@@ -1,9 +1,27 @@
 module Kanan.field;
 
-import Kanan.rsvData;
 
 struct Field {
+  import Kanan.rsvData : rsvFieldData;
+  import std.conv : to;
+  import std.math : abs;
+
   this(rsvFieldData field) {
+    this.width = field.width;
+    this.height = field.height;
+    this.point = field.point;
+    this.startedAtUnixTime = field.startedAtUnixTime;
+    this.turn = field.turn;
+    this.color = field.color;
+    this.agentNum = field.agentNum;
+    this.myTeamID = field.myTeamID;
+    this.myAgentData = field.myAgentData;
+    this.myAreaPoint = field.myAreaPoint;
+    this.myTilePoint = field.myTilePoint;
+    this.rivalTeamID = field.rivalTeamID;
+    this.rivalAgentData = field.rivalAgentData;
+    this.rivalTilePoint = field.rivalTilePoint;
+    this.rivalAreaPoint = field.rivalAreaPoint;
   }
 
   public {
@@ -42,6 +60,68 @@ struct Field {
 
   void calcAreaPoint()
   {
+    myAreaPoint = 0;
 
+    bool[][] areaFlg = new bool[][](height, width);
+    foreach (i; 0 .. height) {
+      foreach (j; 0 .. width) {
+        areaFlg[i][j] = false;
+      }
+    }
+    int[] dx = [-1, 0, 1, 0];
+    int[] dy = [0, 1, 0, -1];
+
+    foreach (i; 1 .. height - 1) {
+      bool startArea = false;
+      int startPos;
+      foreach (j; 1 .. width - 1) {
+        int myTile = 0;
+
+        foreach (k; 0 .. 4) {
+          if ((color[i + dy[k]][j + dx[k]] == myTeamID || areaFlg[i + dy[k]][j + dx[k]]))
+            myTile += color[i][j] != myTeamID ? 1 : 0;
+        }
+
+        if (myTile > 1) {
+          areaFlg[i][j] = true;
+          if (!startArea) {
+            startArea = true;
+            startPos = to!int(j);
+          }
+        }
+
+        if (color[i][j] == myTeamID && startArea) {
+          startArea = false;
+          startPos = to!int(j + 1);
+        }
+
+        if (myTile < 2 && startArea) {
+          areaFlg[i][startPos .. j + 1] = false;
+          startArea = false;
+        }
+      }
+    }
+
+    foreach_reverse (i; 1 .. height - 1) {
+      foreach_reverse (j; 1 .. width - 1) {
+        int myTile = 0;
+        foreach (k; 0 .. 4) {
+          if ((color[i + dy[k]][j + dx[k]] == myTeamID || areaFlg[i + dy[k]][j + dx[k]]))
+            myTile += color[i][j] != myTeamID ? 1 : 0;
+        }
+
+        if (myTile < 4)
+          areaFlg[i][j] = false;
+      }
+    }
+
+    myAreaPoint = 0;
+
+    foreach (i; 0 .. height) {
+      foreach (j; 0 .. width) {
+        if (areaFlg[i][j])
+          myAreaPoint += abs(point[i][j]);
+      }
+    }
   }
 }
