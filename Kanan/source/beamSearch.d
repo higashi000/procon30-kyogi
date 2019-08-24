78fcd89eb3826baf5b3d22a0036caf358858ac6a
higashi000 <higashi.ict@gmail.com>
2019-08-24 22:31:09 +0900

    [fix] Nodeの情報が全て同じになってしまうバグ

--- Kanan/source/beamSearch.d
+++ Kanan/source/beamSearch.d
@@ -1,30 +1,45 @@
 module Kanan.beamSearch;
 
+import std.stdio, std.range, std.algorithm, std.array, std.string, std.math, std.conv,
+       std.container, std.bigint, std.ascii, std.typecons, std.format;
+import core.thread;
 import Kanan.field;
-import std.range;
-import std.stdio;
 
+alias S = Array!(Node*);
 
 struct Node {
+  import std.random : uniform;
+  import Kanan.dispField;
   this(NowField field, uint turn, int[] myMoveDir) {
     this.field = Field(field, myMoveDir);
     this.turn = turn;
     this.evalValue = 0;
   }
 
-  Node* previousNode;
-  Node*[] childNodes;
+  this(NowField field, uint turn) {
+    this.field = Field(field);
+    this.turn = turn;
+    this.evalValue = 0;
+  }
+
+  S childNodes;
 
   int evalValue;
   uint turn;
   Field field;
 
-  Node*[] getNextNodes() {
-    Node*[] ret;
+  S getNextNodes() {
+    import std.random : uniform;
+    S ret;
+    Node* tmp;
 
     foreach (i ; 0 .. 9) {
       foreach (j; 0 .. 9) {
-        ret ~= new Node(field.fieldState, turn + 1, [i, j]);
+        tmp = new Node(field.fieldState, turn + 1, [i, j]);
+        tmp.field.moveAgent();
+        tmp.field.disp;
+        writeln;
+        ret ~= tmp;
       }
     }
 
@@ -33,60 +48,98 @@ struct Node {
 }
 
 class KananBeamSearch {
-  Node* previousNode;
-  Node*[] childNodes;
-  Node*[] searchFinished;
+  import Kanan.dispField;
+  import std.stdio : writeln;
+  import core.thread;
+
+  S childNodes;
+  S grandChildNode;
 
   uint turn;
   Field nowFieldState;
   uint maxTurn;
 
   this(Field nowFieldState, uint turn, uint maxTurn) {
-    this.previousNode = new Node(nowFieldState.fieldState, turn, [0, 0]);
-    this.childNodes = previousNode.getNextNodes();
+    this.childNodes = new Node(nowFieldState.fieldState, turn);
     this.turn = turn;
     this.maxTurn = maxTurn;
     this.nowFieldState = nowFieldState;
   }
 
   void searchAgentAction() {
-    import std.range : front, popFront;
-    import std.algorithm : swap;
-    Node*[] grandChildNode;
+    while (!childNodes.empty()) {
+      Thread.sleep(dur!"seconds"(1));
 
-    while (1) {
-      while (childNodes.length > 0) {
-        auto nowNode = childNodes.front;
-        childNodes.popFront;
-
-        if (maxTurn == nowNode.turn) {
-          searchFinished ~= nowNode;
-          continue;
-        }
-
-        grandChildNode ~= nowNode.getNextNodes();
+      foreach (e; childNodes) {
+        grandChildNode ~= e.getNextNodes;
       }
-      if (grandChildNode.length == 0)
-        break;
-      else
-        swap(childNodes, grandChildNode);
+
+      /* foreach (e; grandChildNode) { */
+      /*   e.field.num.writeln; */
+      /* } */
+
+      childNodes.clear();
+      childNodes.reserve(grandChildNode.length);
+
+      foreach (e; grandChildNode) {
+        childNodes ~= e;
+      }
     }
   }
 }
 
 unittest {
   import Kanan.dispField : disp;
-  import Kanan.connector : KananConnector;
-  auto connector = new KananConnector("127.0.0.1", 8081);
-  connector.getFieldData;
-  auto field = connector.parseFieldData(2);
+
+  NowField field;
+  field.width = 6;
+  field.height = 6;
+  field.point = new int[][](6, 6);
+  field.color = new int[][](6, 6);
+  field.myAgentData = new int[][](2, 3);
+  field.rivalAgentData = new int[][](2, 3);
+  foreach (i; 0 .. 6) {
+    foreach (j; 0 .. 6) {
+      field.point[i][j] = i + j;
+      field.color[i][j] = 0;
+    }
+  }
+
+  field.color[2][2] = 1;
+  field.color[3][3] = 1;
+  field.color[2][3] = 2;
+  field.color[3][2] = 2;
+
+  field.agentNum = 2;
+  field.myTeamID = 1;
+  field.rivalTeamID = 2;
+  field.myAgentData[0][0] = 5;
+  field.myAgentData[0][1] = 2;
+  field.myAgentData[0][2] = 2;
+  field.myAgentData[1][0] = 6;
+  field.myAgentData[1][1] = 3;
+  field.myAgentData[1][2] = 3;
+  field.rivalAgentData[0][0] = 7;
+  field.rivalAgentData[0][1] = 3;
+  field.rivalAgentData[0][2] = 2;
+  field.rivalAgentData[1][0] = 8;
+  field.rivalAgentData[1][1] = 2;
+  field.rivalAgentData[1][2] = 3;
+
+  field.myTilePoint = 0;
+  field.myAreaPoint = 0;
+  field.rivalTilePoint = 0;
+  field.rivalAreaPoint = 0;
+  field.maxTurn = 3;
 
   Field nowField = Field(field);
 
-  auto search = new KananBeamSearch(nowField, 1, 3);
+  auto search = new KananBeamSearch(nowField, 0, 2);
   search.searchAgentAction;
 
-  disp(search.searchFinished[0].field);
-  disp(search.searchFinished[1].field);
-  disp(search.searchFinished[2].field);
+  /* foreach (i; 0 .. search.searchFinished.length) { */
+  /*   disp(search.searchFinished[i].field); */
+  /*   writef("%s", &search.searchFinished[i].field); */
+  /*   writeln; */
+  /* } */
 }
