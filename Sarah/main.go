5e101b73297a5d5d8968a04b2843a37adee2c298
higashi000 <higashi.ict@gmail.com>
2019-09-18 14:37:19 +0900

    [add] フィールドのstructと行動のstruct

---
+++ Sarah/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type FieldData struct {
+	Width             int     `json:"width"`
+	Height            int     `json:"height"`
+	Points            [][]int `json:"points"`
+	StartedAtUnixTime int     `json:"startedAtUnixTime"`
+	Turn              int     `json:"turn"`
+	Tiled             [][]int `json:"tiled"`
+	Teams             []struct {
+		TeamID int `json:"teamID"`
+		Agents []struct {
+			AgentID int `json:"agentID"`
+			X       int `json:"x"`
+			Y       int `json:"y"`
+		} `json:"agents"`
+		TilePoint int `json:"tilePoint"`
+		AreaPoint int `json:"areaPoint"`
+	} `json:"teams"`
+	Actions []interface{} `json:"actions"`
+}
+
+type Actions struct {
+  AgentID int `json:"agentID"`
+  MovePattern string `json:"type"`
+  Dx int `json:"dx"`
+  Dy int `json:"dy"`
+}
+
+func main() {
+}
