a728e5b407af42c4ca16d5fb64966c84cbf38670
higashi000 <higashi.ict@gmail.com>
2019-09-27 17:56:34 +0900

    [add] `FieldData` classにデータをいれるようにした

--- Gui/src/Connector.java
+++ Gui/src/Connector.java
@@ -1,5 +1,7 @@
+import java.awt.print.PrinterGraphics;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.net.Socket;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -44,5 +46,65 @@ public class Connector {
 
     System.out.println(fieldStr);
     return fieldStr;
+  }
+
+  public  static FieldData parseRsvData() {
+    var parseStr = getFieldData().split("\n", 0);
+
+    var tmpParseData = parseStr[0].split(" ", 0);
+    FieldData afterParse = new FieldData(Integer.parseInt(tmpParseData[0]),
+                                         Integer.parseInt(tmpParseData[1]),
+                                         Integer.parseInt(parseStr[5]));
+
+    tmpParseData = parseStr[1].split(";", 0);
+    for (int i = 0; i < afterParse.height; ++i) {
+      var tmp = tmpParseData[i].split(" ", 0);
+      for (int j = 0; j < afterParse.width; ++j) {
+        afterParse.points[i][j] = Integer.parseInt((tmp[j]));
+      }
+    }
+
+    afterParse.startedUnixTime = Integer.parseInt(parseStr[2]);
+    afterParse.turn = Integer.parseInt(parseStr[3]);
+
+    tmpParseData = parseStr[4].split(";", 0);
+    for (int i = 0; i < afterParse.height; ++i) {
+      var tmp = tmpParseData[i].split(" ", 0);
+      for (int j = 0; j < afterParse.width; ++j) {
+        afterParse.color[i][j] = Integer.parseInt((tmp[j]));
+      }
+    }
+
+    afterParse.myTeamID = Integer.parseInt(parseStr[6]);
+
+    tmpParseData = parseStr[7].split(";", 0);
+    for (int i = 0; i < afterParse.agentNum; ++i) {
+      var tmp = tmpParseData[i].split(" ", 0);
+      afterParse.myAgentData[i][0] = Integer.parseInt(tmp[0]);
+      afterParse.myAgentData[i][1] = Integer.parseInt(tmp[1]);
+      afterParse.myAgentData[i][2] = Integer.parseInt(tmp[2]);
+    }
+
+    tmpParseData = parseStr[8].split(" ", 0);
+    afterParse.myTilePoint = Integer.parseInt(tmpParseData[0]);
+    afterParse.myAreaPoint = Integer.parseInt(tmpParseData[1]);
+
+    afterParse.rivalTeamID = Integer.parseInt(parseStr[9]);
+
+    tmpParseData = parseStr[10].split(";", 0);
+    for (int i = 0; i < afterParse.agentNum; ++i) {
+      var tmp = tmpParseData[i].split(" ", 0);
+      afterParse.rivalAgentData[i][0] = Integer.parseInt(tmp[0]);
+      afterParse.rivalAgentData[i][1] = Integer.parseInt(tmp[1]);
+      afterParse.rivalAgentData[i][2] = Integer.parseInt(tmp[2]);
+    }
+
+    tmpParseData = parseStr[11].split(" ", 0);
+    afterParse.rivalTilePoint = Integer.parseInt(tmpParseData[0]);
+    afterParse.rivalAreaPoint = Integer.parseInt(tmpParseData[1]);
+
+    afterParse.maxTurn = Integer.parseInt(parseStr[12]);
+
+    return afterParse;
   }
 }
