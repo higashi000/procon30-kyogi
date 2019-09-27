59798cf9eb0cf7c62483a936f34c7981ac0549cb
higashi000 <higashi.ict@gmail.com>
2019-09-27 20:59:41 +0900

    [add] 回答の送信

---
+++ Gui/src/Action.java
@@ -0,0 +1,14 @@
+public class Action {
+
+  Action(int agentID, String type, int dx, int dy) {
+    this.agentID = agentID;
+    this.type = type;
+    this.dx = dx;
+    this.dy = dy;
+  }
+
+  int agentID;
+  String type;
+  int dx;
+  int dy;
+}
