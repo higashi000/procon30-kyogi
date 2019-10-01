34a3c7c08310af7e09212532ccfcb1f2a7d633e3
harasho0103 <harasho.nao.0103@gmail.com>
2019-10-02 01:04:37 +0900

    [change] フィールドのサイズやエージェントの数をデバッグ用にコンソールから読み取れるようにした

--- sakura/source/hipts_search.d
+++ sakura/source/hipts_search.d
@@ -6,7 +6,7 @@ import std.stdio;
 import std.file;
 
 void main(string[] hoge){
-  int[8][8] nodes = 0;
+
   int range_begin = 0, range_end = 0;
   int[20][20] eval_value = 0;
   auto evl = readText(hoge[1] ~ ".csv");
@@ -20,12 +20,20 @@ void main(string[] hoge){
     }
   }
 
-  // デバッグ用
-  for (int i = 0; i < wh; i++){
-    for (int j = 0; j < wh; j++){
-      writef ("%2d", eval_value[i][j]);
-    }
-    writeln();
+  write ("ageNum : ");
+  auto ageNum = readln.split[0].to!(int);
+
+  string way;
+
+  int[10] age_x;
+  int[10] age_y;
+
+  for (int i = 0; i < ageNum; i++){
+    writef ("myAgent_%d_x : ", i + 1);
+    age_x[i] = readln.split[0].to!(int);
+
+    writef ("myAgent_%d_y : ", i + 1);
+    age_y[i] = readln.split[0].to!(int);
   }
-  
+
 }
