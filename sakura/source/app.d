8898093df33df690d978d2a31d680b32856bfde0
harasho0103 <harasho.nao.0103@gmail.com>
2019-10-06 16:04:32 +0900

    [change] opencsv.dを生やしたことによる調整

--- sakura/source/app.d
+++ sakura/source/app.d
@@ -3,11 +3,13 @@ import std.range;
 import std.conv;
 
 import sakura.guchoku_search;
+import sakura.hipts_search;
 import sakura.opencsv;
 
 void main()
 {
   int[20][20] eval = opencsv(readln.split[0].to!string);
   string ans = guchoku_search(eval);
+  string hipts_ans = hipts_search(eval);
   write (ans);
 }
--- sakura/source/hipts_search.d
+++ sakura/source/hipts_search.d
@@ -6,30 +6,7 @@ import std.stdio;
 import std.file;
 import std.container;
 
-int range_begin = 0, range_end = 0;
-int[20][20] eval;
-int w, h;
-
-void opencsv(){
-  // ファイル名を拡張子無しで打ち込みひらく
-  auto file_head = readln.split[0].to!string;
-  string evl = readText(file_head ~ ".csv");
-
-  // 仮にフィールドが width = 10 , hight = 10 とする
-  w = 10;
-  h = 10;
-
-  // 評価値を配列に格納する
-  for (int i = 0; i < w; i++){
-    for (int j = 0; j < h; j++){
-      while (evl[range_end] != ',') range_end++;
-      eval[i][j] = to!(int)(evl[range_begin..range_end]);
-      range_begin = ++range_end;
-    }
-  }
-}
-
-void hipts_search(){
+void hipts_search(int[20][20] eval){
 
   write ("ageNum : ");
   auto ageNum = readln.split[0].to!(int);
@@ -44,4 +21,6 @@ void hipts_search(){
     writef ("myAgent_%d_y : ", i + 1);
     age_y[i] = readln.split[0].to!(int);
   }
+
+  
 }
