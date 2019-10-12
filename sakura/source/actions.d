0db4ff5819f7e58fb5b357d24899beb539fbb275
harasho0103 <harasho.nao.0103@gmail.com>
2019-10-12 18:01:34 +0900

    [add] connector.dのために丸パクリしたもの

---
+++ sakura/source/actions.d
@@ -0,0 +1,14 @@
+module actions;
+
+struct Actions {
+  this(int agentID, string type, int dx, int dy) {
+    this.agentID = agentID;
+    this.type = type;
+    this.dx = dx;
+    this.dy = dy;
+  }
+  int agentID;
+  string type;
+  int dx;
+  int dy;
+}
---
+++ sakura/source/rsvData.d
@@ -0,0 +1,19 @@
+module sakura.rsvData;
+
+struct rsvFieldData {
+  int width;
+  int height;
+  int[][] point;
+  int startedAtUnixTime;
+  int turn;
+  int[][] color;
+  int agentNum;
+  int myTeamID;
+  int[][] myAgentData;
+  int myTilePoint;
+  int myAreaPoint;
+  int rivalTeamID;
+  int[][] rivalAgentData;
+  int rivalTilePoint;
+  int rivalAreaPoint;
+}
