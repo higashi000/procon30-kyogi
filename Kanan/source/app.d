cdf0d6292e4812dd69f0559bde7f90f83fa9a83a
higashi000 <higashi.ict@gmail.com>
2019-10-12 21:40:05 +0900

    [update] mainループ

--- Kanan/source/app.d
+++ Kanan/source/app.d
@@ -23,43 +23,9 @@ void main(string[] args)
     writeln(field.myTilePoint + field.myAreaPoint);
     writeln(field.rivalTilePoint + field.rivalAreaPoint);
 
-    /* if (turn < maxTurn / 2) { */
-    /*   auto beamSearch = new KananBeamSearch(field, turn, maxTurn, 20, 9 ^^ 3); */
-    /*   beamSearch.searchAgentAction(); */
-    /*   auto answer = beamSearch.bestAnswer(); */
-    /*   connector.sendResult(answer); */
-    /* } else { */
-
-
-    // 人力卍 --- {{{
-      Actions[] human;
-      int[] dx = [0, -1, -1, 0, 1, 1, 1, 0, -1];
-      int[] dy = [0, 0, -1, -1, -1, 0, 1, 1, 1];
-
-      foreach (i; 0 .. field.agentNum) {
-        int humanPower;
-        int tmp;
-        string humanMove;
-        writef("エージェント方向 >> ");
-        readf("%d\n", &humanPower);
-        writef("エージェント動き (1: move, 2: stay, 3: remove)>> ");
-        readf("%d\n", tmp);
-        if (tmp == 1)
-          humanMove = "move";
-        else if (tmp == 2)
-          humanMove = "stay";
-        else
-          humanMove = "remove";
-        human ~= Actions(i + 4, humanMove, dx[humanPower], dy[humanPower]);
-      }
-  //}}}
-
-      auto montecarlo = new MontecarloTreeSearch(field, turn, maxTurn, 4500, 20);
-      auto answer = montecarlo.playGame();
-      connector.sendResult(answer);
-      Thread.sleep(dur!("seconds")(3));
-      connector.sendHumanResult(human);
-    /* } */
+    auto montecarlo = new MontecarloTreeSearch(field, turn, maxTurn, 4500, 20);
+    auto answer = montecarlo.playGame();
+    connector.sendResult(answer);
 
 
     turn++;
