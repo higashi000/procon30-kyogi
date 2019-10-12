c785173e7dc4f85ba984b0354479f9a594940f9c
higashi000 <higashi.ict@gmail.com>
2019-10-12 22:54:17 +0900

    [fix] 全部起動しないのを直した

--- start.sh
+++ start.sh
@@ -8,11 +8,17 @@ echo -n "最大ターン数 >>"
 read _maxTurn
 echo -n "自チームID >>"
 read _myTeamID
+echo -n "運営鯖 >> "
+read _uneisaba
+echo -n "thinking time >> "
+read _thinkingTime
 
 # ローカルサーバーの起動
 cd ./Mari/
-./main $_mariIP $_mariPort $_myTeamID $_maxTurn &
+gnome-terminal -- ./main $_mariIP $_mariPort $_myTeamID $_maxTurn $_uneisaba $_thinkingTime &
 cd ..
+
+sleep 1
 
 # solverの起動
 cd ./Kanan
